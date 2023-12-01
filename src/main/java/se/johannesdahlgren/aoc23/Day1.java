9ab7c3414c0f9ade2794ad298949e8c438034461
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2023-12-01 12:47:33 +0100

    Day1 part 2

--- src/main/java/se/johannesdahlgren/aoc23/Day1.java
+++ src/main/java/se/johannesdahlgren/aoc23/Day1.java
@@ -7,6 +7,8 @@ import se.johannesdahlgren.aoc23.util.FileReader;
 
 public class Day1 {
 
+  private final List<String> words = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+
   public int sumOfCalibrationValues(String fileName) {
     List<String> calibrationDocument = FileReader.readFileAsLines(fileName);
     return calibrationDocument.stream()
@@ -14,17 +16,65 @@ public class Day1 {
         .reduce(0, Integer::sum);
   }
 
+  public int sumOfCalibrationValuesWithWords(String fileName) {
+    List<String> calibrationDocument = FileReader.readFileAsLines(fileName);
+    return calibrationDocument.stream()
+        .map(this::getCalibrationValueWithWords)
+        .reduce(0, Integer::sum);
+  }
+
   private int getCalibrationValue(String s) {
     Pattern pattern = Pattern.compile("\\d");
     Matcher matcher = pattern.matcher(s);
     StringBuilder sj = new StringBuilder();
-    while (matcher.find()){
+    while (matcher.find()) {
       sj.append(s.charAt(matcher.start()));
     }
     String string = sj.toString();
     char firstMatch = string.charAt(0);
     char lastMatch = string.charAt(sj.length() - 1);
     return Integer.parseInt(firstMatch + String.valueOf(lastMatch));
+  }
+
+  private int getCalibrationValueWithWords(String s) {
+    int first = getFirstDigit(s);
+    int last = getLastDigit(s);
+    return Integer.parseInt(first + String.valueOf(last));
+  }
+
+  private int getFirstDigit(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      int c = getInt(s, i);
+      if (c != -1) {
+        return c;
+      }
+    }
+    return 0;
+  }
+
+  private int getLastDigit(String s) {
+    for (int i = s.length() - 1; i >= 0; i--) {
+      int c = getInt(s, i);
+      if (c != -1) {
+        return c;
+      }
+    }
+    return 0;
+  }
+
+  private int getInt(String s, int i) {
+    char c = s.charAt(i);
+    if (Character.isDigit(c)) {
+      return c - '0';
+    } else {
+      for (int j = 0; j < words.size(); j++) {
+        String word = words.get(j);
+        if (s.substring(i).startsWith(word)) {
+          return j + 1;
+        }
+      }
+    }
+    return -1;
   }
 
 }
--- src/test/java/se/johannesdahlgren/aoc23/Day1Test.java
+++ src/test/java/se/johannesdahlgren/aoc23/Day1Test.java
@@ -20,4 +20,18 @@ class Day1Test {
     int sumOfCalibrationValues = day1.sumOfCalibrationValues("day1.txt");
     assertThat(sumOfCalibrationValues, is(55208));
   }
+
+  @Test
+  void day1Part2Example() {
+    Day1 day1 = new Day1();
+    int sumOfCalibrationValues = day1.sumOfCalibrationValuesWithWords("day1example2.txt");
+    assertThat(sumOfCalibrationValues, is(281));
+  }
+
+  @Test
+  void day1Part2() {
+    Day1 day1 = new Day1();
+    int sumOfCalibrationValues = day1.sumOfCalibrationValuesWithWords("day1.txt");
+    assertThat(sumOfCalibrationValues, is(54578));
+  }
 }
