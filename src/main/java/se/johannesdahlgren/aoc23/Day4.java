93bf70520f0fd6e4f73ab35fe533b808e9dc5edd
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2023-12-04 20:40:05 +0100

    Day 4 part 2, naive takes 12s. I could filter out non-winning cards first probably

--- src/main/java/se/johannesdahlgren/aoc23/Day4.java
+++ src/main/java/se/johannesdahlgren/aoc23/Day4.java
@@ -3,6 +3,7 @@ package se.johannesdahlgren.aoc23;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import se.johannesdahlgren.aoc23.util.FileReader;
 
@@ -18,10 +19,38 @@ public class Day4 {
     List<String> scratchCardTable = FileReader.readFileAsLines(fileName);
     return scratchCardTable.stream()
         .map(this::toScratchCards)
-        .map(this::keepMyWinningNumbers)
+        .map(ScratchCard::keepMyWinningNumbers)
         .filter(Predicate.not(List::isEmpty))
         .map(this::doubleNumbers)
         .reduce(0, Integer::sum);
+  }
+
+  public int numberOfScratchCards() {
+    List<String> scratchCardTable = FileReader.readFileAsLines(fileName);
+    List<ScratchCard> scratchCards = scratchCardTable.stream()
+        .map(this::toScratchCards)
+        .toList();
+
+    ArrayList<ScratchCard> duplicatedScratchCards = new ArrayList<>(scratchCards);
+    for (int i = 1; i <= duplicatedScratchCards.size(); i++) {
+      ScratchCard scratchCard = duplicatedScratchCards.get(i - 1);
+      if (scratchCard.isWinningCard()) {
+        List<Integer> keepMyWinningNumbers = scratchCard.keepMyWinningNumbers();
+        for (int j = 1; j <= keepMyWinningNumbers.size(); j++) {
+          int nextCardNumber = scratchCard.cardNumber + j;
+          int finalJ = j;
+          Optional<ScratchCard> nextScratchCardOptional = scratchCards.stream()
+              .filter(c -> c.cardNumber == scratchCard.cardNumber + finalJ)
+              .findFirst();
+          if (nextScratchCardOptional.isPresent()) {
+            ScratchCard nextScratchCard = nextScratchCardOptional.get();
+            duplicatedScratchCards.add(new ScratchCard(nextCardNumber, nextScratchCard.winningNumbers, nextScratchCard.myNumbers));
+          }
+        }
+      }
+    }
+
+    return duplicatedScratchCards.size();
   }
 
   private int doubleNumbers(List<Integer> myWinningNumbers) {
@@ -32,17 +61,14 @@ public class Day4 {
   }
 
   private ScratchCard toScratchCards(String line) {
-    String[] split = line.substring(line.indexOf(":") + 1).split("\\|");
+    int colonIndex = line.indexOf(":");
+    int cardNumber = Integer.parseInt(line.substring(5, colonIndex).trim());
+    String[] split = line.substring(colonIndex + 1).split("\\|");
     List<Integer> winningNumbers = stringToNumberList(split[0]);
     List<Integer> myNumbersNumbers = stringToNumberList(split[1]);
-    return new ScratchCard(winningNumbers, myNumbersNumbers);
+    return new ScratchCard(cardNumber, winningNumbers, myNumbersNumbers);
   }
 
-  private List<Integer> keepMyWinningNumbers(ScratchCard scratchCard) {
-    ArrayList<Integer> myWinningNumbers = new ArrayList<>(scratchCard.myNumbers);
-    boolean retained = myWinningNumbers.retainAll(scratchCard.winningNumbers);
-    return retained ? myWinningNumbers : List.of();
-  }
 
   private static List<Integer> stringToNumberList(String stringOfNumbers) {
     return Arrays.stream(stringOfNumbers.
@@ -52,7 +78,16 @@ public class Day4 {
         .toList();
   }
 
-  private record ScratchCard(List<Integer> winningNumbers, List<Integer> myNumbers) {
+  private record ScratchCard(int cardNumber, List<Integer> winningNumbers, List<Integer> myNumbers) {
 
+    public List<Integer> keepMyWinningNumbers() {
+      ArrayList<Integer> myWinningNumbers = new ArrayList<>(myNumbers);
+      boolean retained = myWinningNumbers.retainAll(winningNumbers);
+      return retained ? myWinningNumbers : List.of();
+    }
+
+    public boolean isWinningCard() {
+      return !keepMyWinningNumbers().isEmpty();
+    }
   }
 }
--- src/test/java/se/johannesdahlgren/aoc23/Day4Test.java
+++ src/test/java/se/johannesdahlgren/aoc23/Day4Test.java
@@ -20,4 +20,19 @@ class Day4Test {
     int points = day4.pointsOfWinningNumbers();
     assertThat(points, is(23235));
   }
+
+
+  @Test
+  void part2Example() {
+    Day4 day4 = new Day4("day4example.txt");
+    int numberOfScratchCards = day4.numberOfScratchCards();
+    assertThat(numberOfScratchCards, is(30));
+  }
+
+  @Test
+  void part2() {
+    Day4 day4 = new Day4("day4.txt");
+    int numberOfScratchCards = day4.numberOfScratchCards();
+    assertThat(numberOfScratchCards, is(5920640));
+  }
 }
